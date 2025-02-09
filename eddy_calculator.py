09f24688562dc15c18346c7c175c22394c8cbaba
eddydesouza <eddypsc@gmail.com>
2025-02-09 11:59:10 -0500

    Main application window

---
+++ eddy_calculator.py
@@ -0,0 +1,13 @@
+# Import the tkinter module
+import tkinter as tk
+
+# Create the main application window
+root = tk.Tk()
+root.title("Simple Calculator")
+
+# Allow window resizing
+root.geometry("300x400") # Initial window size
+
+
+
+
